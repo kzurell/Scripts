134f45607f08e10eebcb48e1feba3e58112a5a2a
Kirk Zurell <kirk@kirk.zurell.name>
2014-08-20 13:36:11 -0400

    Initial version for Hello World
    Initial version for Hello World, testing metadata.

---
+++ Userscripts/Open_Chromecast_Developer_Tools.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name            Open Chromecast Developer Tools
+// @description     Open the Chromecast Developer Tools quickly and automatically.
+// @icon            http://www.kirk.zurell.name/favicon.ico
+//
+// @author          Kirk Zurell <kirk@kirk.zurell.name>
+// @namespace       http://kirk.zurell.name/userscripts
+// downloadURL      https://raw.githubusercontent.com/kzurell/Scripts/master/Userscripts/Open_Chromecast_Developer_Tools.user.js
+//
+// @license         Use at your own risk.
+// @copyright       Copyright (C) 2014, by Kirk Zurell
+//
+// @include         *
+// include         http://10.0.1.*:9222
+//
+// @version         0.1
+// @updateURL       https://raw.githubusercontent.com/kzurell/Scripts/master/Userscripts/Open_Chromecast_Developer_Tools.user.js
+//
+// @run-at          document-start
+// resource        resourceName http://www.example.com/example.png
+// unwrap
+// ==/UserScript==
+ 
+/**
+ * Use at your own risk.
+ */
+ 
+/**
+ * SCRIPT DESCRIPTION.
+ *
+ * @see http://wiki.greasespot.net/API_reference
+ * @see http://wiki.greasespot.net/Metadata_Block
+ */
+(function() {	
+  console.log("Starting Open Chromecast Developer Tools");
+    
+})();
