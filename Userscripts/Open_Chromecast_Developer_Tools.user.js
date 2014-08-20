c8b043cbd26ceb68a4f7ab45f1fc474bcc5195c8
Kirk Zurell <kirk@kirk.zurell.name>
2014-08-20 14:02:57 -0400

    First try at link click
    First try at link click.

--- Userscripts/Open_Chromecast_Developer_Tools.user.js
+++ Userscripts/Open_Chromecast_Developer_Tools.user.js
@@ -11,9 +11,9 @@
 // @copyright       Copyright (C) 2014, by Kirk Zurell
 //
 // @include         *
-// include         http://10.0.1.*:9222
+// include         http://10.0.1.2:9222
 //
-// @version         0.1
+// @version         0.2
 // @updateURL       https://raw.githubusercontent.com/kzurell/Scripts/master/Userscripts/Open_Chromecast_Developer_Tools.user.js
 //
 // @run-at          document-start
@@ -23,6 +23,9 @@
  
 /**
  * Use at your own risk.
+ *
+ * Cribbed from http://stackoverflow.com/questions/12375008/how-can-my-userscript-get-a-link-based-on-the-links-text
+
  */
  
 /**
@@ -33,5 +36,25 @@
  */
 (function() {	
   console.log("Starting Open Chromecast Developer Tools");
+
+//--- Note that contains() is CASE SENSITIVE.
+var specLinks   = document.evaluate (
+    "//a[contains(text(), 'Chromecast Receiver')]",
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null
+);
+
+console.dir(specLinks);
+
+if (specLinks.snapshotLength) {
+    console.log("Opening");
+    var specLink    = specLinks.snapshotItem (0);
+    var clickEvent  = document.createEvent ('MouseEvents');
+    clickEvent.initEvent ('click', true, true);
+    specLink.dispatchEvent (clickEvent);
+}
+
     
 })();
